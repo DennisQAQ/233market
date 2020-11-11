1e364f710933e111a7b777537061f6e188c57884
Dennis Wang <DennisEverglow@gmail.com>
2020-11-11 14:24:10 +0800

    测试feign远程调用功能

---
+++ market-member/src/main/java/com/roc/market/member/feign/CouponFeignService.java
@@ -0,0 +1,11 @@
+package com.roc.market.member.feign;
+
+import com.roc.market.commons.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("market-coupon")
+public interface CouponFeignService {
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
