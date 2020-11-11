480d8721bc96a21131465a0f6a590e8af27e50f9
Dennis Wang <DennisEverglow@gmail.com>
2020-11-11 13:04:55 +0800

    开始注册服务发现功能

--- market-coupon/src/main/java/com/roc/market/coupon/MarketCouponApplication.java
+++ market-coupon/src/main/java/com/roc/market/coupon/MarketCouponApplication.java
@@ -2,7 +2,9 @@ package com.roc.market.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //开启nacos注册中心注解
 @SpringBootApplication
 public class MarketCouponApplication {
 
--- market-product/src/main/java/com/roc/market/product/MarketProductApplication.java
+++ market-product/src/main/java/com/roc/market/product/MarketProductApplication.java
@@ -3,7 +3,9 @@ package com.roc.market.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.roc.market.product.dao")
 @SpringBootApplication
 public class MarketProductApplication {
